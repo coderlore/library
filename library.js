9d251e8322a700731d1dd0db4eba209dc8146771
coderlore <64398168+coderlore@users.noreply.github.com>
2021-07-25 22:29:13 -1000

    Added form values

--- library.js
+++ library.js
@@ -1,11 +1,16 @@
-let myLibrary = [];
-const remove = document.getElementById('remove');
+const title = document.getElementById("title");
+const button = document.getElementById("button");
 
-function Book(author, title, pages, readOrNot) {
-    this.autor = author;
-    this.title = title;
-    this.pages = pages;
-    this.readOrNot = readOrNot;
+let myLibrary = [];
+//const remove = document.getElementById('remove');
+
+class Book {
+    constructor(author, title, pages, readOrNot) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.readOrNot = readOrNot;
+    }
     /*remove.addEventListner('click', () => {
         removeBook();
     })*/
@@ -19,5 +24,20 @@ function populateStorage() {
     localStorage.setItem('book', 0);
 }
 
-const book = new Book('JK','Harry Potter','400','read');
-console.log(book.title) 
+//const book = new Book('JK','Harry Potter','400','read');
+//console.log(book.title)
+//console.log(book.author) 
+//console.log(book.pages) 
+//console.log(book.readOrNot) 
+
+form = document.getElementById('form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let pages = document.getElementById('pages').value 
+    console.log(title);
+    console.log(author);
+    console.log(pages);
+})
